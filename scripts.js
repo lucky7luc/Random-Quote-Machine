2a570826604eb2ab513d96762d236fdd7605ec8e
Lucas Herrmann <92154740+lucky7luc@users.noreply.github.com>
2024-08-29 16:40:23 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,85 +1,58 @@
+let quotesArr = [];
+let currentAuthor = '';
+let currentQuote = '';
+let randomQuote = '';
 
+let colors = [
+    "#8B4513", // Sattelbraun
+    "#556B2F", // Dunkles Olivgrün
+    "#4682B4", // Stahlblau
+    "#708090", // Schiefergrau
+    "#6B8E23", // Olivgrün
+    "#2F4F4F", // Dunkles Schiefergrau
+    "#8FBC8F", // Dunkles Seegrün
+    "#A52A2A", // Braun
+    "#D2B48C", // Bräunlich
+    "#B0C4DE"  // Hellblau     
+];
 
-    let quotesData;
-    let currentQuote = '';
-    let currentAuthor = '';
-
-    const colors = [
-            "#8B4513", // Sattelbraun
-            "#556B2F", // Dunkles Olivgrün
-            "#4682B4", // Stahlblau
-            "#708090", // Schiefergrau
-            "#6B8E23", // Olivgrün
-            "#2F4F4F", // Dunkles Schiefergrau
-            "#8FBC8F", // Dunkles Seegrün
-            "#A52A2A", // Braun
-            "#D2B48C", // Bräunlich
-            "#B0C4DE"  // Hellblau        
-    ];
-
-    function getQuotes() {
-        return $.ajax({                                         //jQuery's $.ajax Methode wird verwendet, um eine HTTP-Anfrage an die angegebene URL zu senden.
-            headers: { Accept: 'application/json' },           // Akzeptiert ein json 
-            url: 'https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json',
-            success: function (jsonQuotes){                     // success wird ausgeführt, wenn ajax anfrage erfolgreich ist
-                if(typeof jsonQuotes === 'string'){
-                    quotesData = JSON.parse(jsonQuotes);
-                    console.log('quotesData');
-                    console.log(quotesData);
-                }
-            }
-        });
+const fetchQuotes = async () => {
+    try {
+        const response = await fetch("https://gist.githubusercontent.com/camperbot/5a022b72e96c4c9585c32bf6a75f62d9/raw/e3c6895ce42069f0ee7e991229064f167fe8ccdc/quotes.json");
+        const result = await response.json();
+        quotesArr = result.quotes;
+        console.log(quotesArr);
+        getRandomQuotes();
+    } catch (e) {
+        console.log("Error fetching the quotes", e);
     }
+};
+fetchQuotes();
 
-    function getRandomQuote() {
-        return quotesData.quotes[
-            Math.floor(Math.random() * quotesData.quotes.length)
-        ];
-    }
+const getRandomQuotes = () => {
+    const randomIndex = Math.floor(Math.random() * quotesArr.length);
+    randomQuote = quotesArr[randomIndex];
+    currentQuote = randomQuote.quote;
+    currentAuthor = randomQuote.author;
+    updateUI();
+    getRandomColor();
+}
 
-    function getQuote() {
-        let randomQuote = getRandomQuote();
-        
-        currentQuote = randomQuote.quotes;
-        currentAuthor = randomQuote.author;
-
-        $('#tweet-quote').attr(                     // Gibt dem #tweet-quote den href Attribut 
-            'href',                                 // hanshtag quotes, related Parameter schlägt vor mir zu folgen. text definiert den Inhalt
-            'https://twitter.com/intent/tweet?hashtags=quotes&related=LHerrmann96&text=' +  
-              encodeURIComponent('"' + randomQuote.quote + '"   ' + "- " + currentAuthor)         // hier wird der Text und Author wieder gegeben
-          ); 
-
-        
-        $('#text').animate({ opacity: 0 }, 500, function () {
-            $(this).animate({ opacity: 1 }, 500);
-            $('#text').text('"' + randomQuote.quote + '"');
-          });
-
-        $("#author").animate({ opacity: 0 }, 500, function () {
-            $(this).animate({ opacity: 1 }, 500);
-            $("#author").html("- " + randomQuote.author);
-        });  
+const getRandomColor = () => {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    const randomColor = colors[randomIndex];
+    $("body").css("background-color", randomColor);
+}
 
 
-        let randomColor = Math.floor(Math.random() * colors.length);
-       $("html body").animate(
-        {
-            backgroundColor: colors[randomColor],
-            color: colors[randomColor]
-       }, 1000
-       );
-       $("button").animate(
-        {
-            backgroundColor: colors[randomColor],
-        }, 1000
-       );
-    }
+const updateUI = () => {
+    $("#text").text('"' + currentQuote + '"');
+    $("#author").text('- ' + currentAuthor);
 
+    const retweetLink = `https://twitter.com/intent/tweet?text=${encodeURIComponent(`"${currentQuote}" - ${currentAuthor}`)}`;
+    $("#tweet-quote").attr("href", retweetLink);
+}
 
-$(document).ready(function(){
-    getQuotes().then(() => {
-        getQuote();
-      });
-    
-      $('#new-quote').on('click', getQuote);
+$(document).ready(function() {
+    $("#new-quote").click(getRandomQuotes);
 });
